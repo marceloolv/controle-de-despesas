e40168335439428ae76fc26f07bf24d6c191a696
Ladislau <ladislaumartins@gmail.com>
2026-08-05 21:01:55 -0400

    feat:correcao e ajuste

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import menu
 import lancamentos
-import consultas
-import orcamentos
-import relatorios
 
 
 def iniciar_sistema():
     while True:
-        # Chama a função do arquivo menu.py
         opcao = menu.exibir_menu()
 
-        if opcao == "0":
+        if opcao == "1":
+            lancamentos.lancamentos()
+
+        elif opcao == "2":
+            pass
+
+        elif opcao == "3":
+            pass
+
+        elif opcao == "4":
+            pass
+
+        elif opcao == "0":
             print("\nEncerrando o sistema. Até logo!")
             break
-        elif opcao == "1":
-           ...# lancamentos.executar()
-        elif opcao == "2":
-           ... # consultas.executar()
-        elif opcao == "3":
-           ...# orcamentos.executar()
-        elif opcao == "4":
-           ... # relatorios.executar()
+
         else:
             print("\nOpção inválida! Tente novamente.")
 
